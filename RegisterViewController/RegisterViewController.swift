d653f605090d9d7792833835f537cc3b3494fa7e
Nilson José <nilson.saballos@tecnacional.edu.ni>
2025-01-14 00:05:09 -0500

    Agregar nombre y foto de Registro de datos de universidad

--- RegisterViewController/RegisterViewController.swift
+++ RegisterViewController/RegisterViewController.swift
@@ -5,20 +5,45 @@
 //  Created by User-UAM on 11/16/24.
 //
 import UIKit
+import SwiftUI
 
 class RegisterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .white
+        //Color de fondo
+        self.view.backgroundColor = UIColor(red: 128/255, green: 204/255, blue: 198/255, alpha: 1)
         
-        // Configurar la vista
-        let label = UILabel()
-        label.text = "Register View"
-        label.frame = CGRect(x: 100, y: 200, width: 200, height: 50)
-        label.textAlignment = .right
+        //Titulo
+        let titleLabel = UILabel()
+        titleLabel.text = "Registro"
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 30)
+        titleLabel.textColor = .white
+        titleLabel.textAlignment = .center
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(titleLabel)
         
-        self.view.addSubview(label)
+        
+        //Imagen Jaguar
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "image")
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(imageView)
+        
+        //configuración Auto Layout
+        NSLayoutConstraint.activate([
+            //Titulo
+            titleLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            titleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            
+            //Imagen
+            imageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            imageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 150),
+            imageView.heightAnchor.constraint(equalToConstant: 150)
+        ])
+
     }
 }
